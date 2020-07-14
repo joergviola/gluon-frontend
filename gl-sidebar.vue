d719a6720e8269759a91af0e50c23e08c19e0474
joergviola <joerg@joergviola.de>
2020-07-14 21:48:51 +0200

    group-reduce and cleanup

---
+++ gl-sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+   <div style="display: flex; flex-direction: column; height: 100%; background-color:#666666">
+      <router-menu />
+      <router-menu :bottom="true" />
+      <div style="text-align: center; font-size: 70%; padding-bottom: 10px">
+         <router-link to="/version"><span>gluon 08.07.20</span></router-link>
+      </div>
+   </div>
+</template>
+
+<script>
+import RouterMenu from './gl-routerMenu'
+
+export default {
+    name: "Sidebar",
+    components: {RouterMenu},
+}
+</script>
